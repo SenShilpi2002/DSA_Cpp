7bc355c2014618dab6e4ce3c615402ed71afddd0
Shilpi Sen <shilpisen072002@gmail.com>
2023-01-04 23:42:50 +0530

    day4

---
+++ ARRAYS/avarage.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int arr[10];
+    int sum=0;
+    int avg;
+    cout<<"Enter the array elements : "<<endl;
+    for(int i=0;i<9;i++){
+        cin>>arry[i];
+        sum=sum+arr[i];
+    }
+    cout<<"The sum of the arry elements "<<sum<<endl;
+    avg=sum/10;
+    cout<<"The average of the array elements : "<<avg<<endl;
+}
