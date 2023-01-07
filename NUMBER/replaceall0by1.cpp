7ef0f3c2259c8f4ad786782c454013b88b969dfe
Shilpi Sen <shilpisen072002@gmail.com>
2023-01-07 23:44:43 +0530

    day 6

---
+++ NUMBER/replaceall0by1.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int n,digit;
+    vector<int>v;
+    
+    cout<<"Enter a number : "<<endl;
+    cin>>n;
+    while(n!=0){
+        digit=n%10;
+            if(digit==0){
+
+            }
+    }
+}
---
+++ STRINGS/removevowel.cpp
@@ -0,0 +1,8 @@
+#include<iostream>
+#include<string.h>
+using namespace std;
+int main(){
+    int size=0;
+    char str[30]="shilpi is a kutti";
+    size=strlen(str);
+}
